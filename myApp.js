a69d4382953e8478f5c0f1fc6e8d92598d19f9bb
Manogna <manovad7424@gmail.com>
2016-10-02 00:45:38 -0500

    Add files via upload

---
+++ myApp.js
@@ -0,0 +1,9 @@
+/**
+ * Created by manogna on 10/2/2016.
+ */
+
+var app=angular.module('myApp',[]);
+app.controller('myCtrl',function($scope){
+    $scope.firstname= "John";
+    $scope.lastname= "Leadley";
+});
